ecdebed168b740546936f0f0ab5e47e42fd1ff0c
Sophie Slagle <slagsoph@isu.edu>
2024-01-15 15:02:16 -0800

    reactive stacked text added

--- portfolio/react_portfolio/src/components/Intro.jsx
+++ portfolio/react_portfolio/src/components/Intro.jsx
@@ -15,9 +15,11 @@ const Intro = () => {
                 Your browser does not support the video tag.
             </video>
 
-            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-white">
-            <h1 className="text-4xl font-bold">Sophie Slagle</h1>
-            <h1 className="text-4xl font-bold">Web Development</h1>
+            <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white">
+                <div className="w-full text-center">
+                    <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-4">Sophie Slagle</h1>
+                    <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">Web Development</h2>
+                </div>
             </div>
         </div>
     );
