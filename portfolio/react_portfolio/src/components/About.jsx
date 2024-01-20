4587c7577f33ffd26ce86c96557f1434d098e004
Sophie Slagle <Slagsoph@isu.edu>
2024-01-19 23:19:13 -0800

    changed text color for paragraphs to gray

--- portfolio/react_portfolio/src/components/About.jsx
+++ portfolio/react_portfolio/src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div className="bg-black mx-0 flex flex-col items-center justify-center h-[500px] p-4 sm:p-8 md:p-16 lg:p-20 xl:p-24">
             <p className='text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl font-bold mb-4'>About Me</p>
-            <p className='text-gray-700 text-center text-sm sm:text-base md:text-lg lg:text-base xl:text-lg'>
+            <p className='text-gray-200 text-center text-sm sm:text-base md:text-lg lg:text-base xl:text-lg'>
                 Hello, I'm Sophie Slagle, a Full Stack Developer located in Lakewood, CA, specializing in front-end development with React.js, Tailwind CSS, and cutting-edge technologies. I'm dedicated to crafting seamless user experiences and staying current with industry trends. My expertise includes front-end technologies like React.js, Tailwind CSS, CSS, and Bootstrap. I'm proficient in HTML, JavaScript, and have experience with frameworks and libraries such as React, Flask, and Django. I'm currently contributing as a Front-end Developer Intern at Velosaty, actively involved in feature development, bug resolution, and code optimization. I hold a Certificate in Full Stack Software Development from Coding Dojo, a Master of Arts in Sport Management from the University of South Dakota, and a Bachelor of Science in Physical Education from Idaho State University. Connect with me!
             </p>
         </div>
--- portfolio/react_portfolio/src/components/Intro.jsx
+++ portfolio/react_portfolio/src/components/Intro.jsx
@@ -18,7 +18,7 @@ const Intro = () => {
                     <h3 className="text-2xl sm:text-3xl md:text-4xl lg:text-3xl font-bold mb-4">Hello, I am</h3>
                     <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-4">Sophie Slagle</h1>
                     <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-8">Web Development</h2>
-                    <p className="hidden md:flex text-1l sm:text-1l md:text-2l lg:text-3l ">I am a results-driven Full Stack Developer specializing in front-end development with React.js, Tailwind CSS, and cutting-edge technologies. Committed to creating seamless user experiences and passionate about staying current with industry trends.</p>
+                    <p className="text-gray-200 hidden md:flex text-1l sm:text-1l md:text-2l lg:text-3l ">I am a results-driven Full Stack Developer specializing in front-end development with React.js, Tailwind CSS, and cutting-edge technologies. Committed to creating seamless user experiences and passionate about staying current with industry trends.</p>
                 </div>
             </div>
         </div>
