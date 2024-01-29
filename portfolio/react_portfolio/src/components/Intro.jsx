031365e7cd242205acba235ff0ff831b7a36475b
Sophie Slagle <slagsoph@isu.edu>
2024-01-29 15:14:34 -0800

    spelling

--- portfolio/react_portfolio/src/components/Intro.jsx
+++ portfolio/react_portfolio/src/components/Intro.jsx
@@ -6,7 +6,7 @@ import bg from '../images/background.mp4';
 
 const Intro = () => {
     return (
-        <div className="video-background relative md:h-[550px] h-[400px] md:top-0 md:mt-0">
+        <div className="video-background relative md:h-[600px] h-[400px] md:top-0 md:mt-0">
             <video className="w-full h-full object-cover" autoPlay loop muted playsInline>
                 <source src={bg} type="video/mp4" />
                 Your browser does not support the video tag.
@@ -17,7 +17,7 @@ const Intro = () => {
                 <div className="w-full text-center sm:text-left">
                     <h3 className="text-2xl sm:text-3xl md:text-4xl lg:text-3xl font-bold mb-4">Hello, I am</h3>
                     <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-4">Sophie Slagle</h1>
-                    <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-8">Web Development</h2>
+                    <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-8">Web Developer</h2>
                     <p className="text-gray-200 hidden md:flex text-1l sm:text-1l md:text-2l lg:text-3l ">I am a results-driven Full Stack Developer specializing in front-end development with React.js, Tailwind CSS, and cutting-edge technologies. Committed to creating seamless user experiences and passionate about staying current with industry trends.</p>
                 </div>
             </div>
